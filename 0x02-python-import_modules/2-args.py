50cc131affcbce4816f9db400cd0a48c70810047
Twhite2 <emmanuelmieye@gmail.com>
2022-06-30 13:40:35 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
 
-    if i == 0:
-        print("{} arguments:".format(i))
-    elif i == 1:
-        print("{} arguments:".format(i))
+    from sys import argv
+    size = len(argv)
+    if size > 2:
+        print("{:d} arguments:".format(size - 1))
+    elif size == 1:
+        print("0 arguments.")
     else:
-        print("{} arguments:"format(i))
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+        print("1 argument:")
+    for i in range(1, size):
+        print("{:d}: {:s}".format(i, argv[i]))
