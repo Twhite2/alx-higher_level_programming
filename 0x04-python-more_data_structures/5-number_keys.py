c9af31cbef0e7a0b1aa035117cbc87446e13783e
Twhite2 <emmanuelmieye@gmail.com>
2022-07-06 12:33:31 -0700

    task5 update

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
-
-    for i in list_keys:
-        num += 1
+    if isinstance(a_dictionary, dict):
+        return len(a_dictionary.keys())
