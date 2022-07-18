a8d2d3d41f656827ffee4458000ee88573dde303
Twhite2 <emmanuelmieye@gmail.com>
2022-07-18 13:02:59 -0700

    task0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """PRINT X ELEMENTS OF A LIST"""
+
+    ret = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+Read me for 0x05-python-exceptions
